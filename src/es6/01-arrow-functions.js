813f9e87ad832ac90b987f031b313bffe0693ebb
Randy Moya <stevenmoya02@gmail.com>
2023-08-28 19:34:10 -0600

    Se crearon los archivos 00 01 y 02 en los cuales hay ejemplos de las key words const, let y var, sober las arrow functions y sobre los template literals

---
+++ src/es6/01-arrow-functions.js
@@ -0,0 +1,13 @@
+//esta es un funcion normal
+function square(num) {
+    return num * num;
+}
+
+
+//esta es una arrow function
+const square = (num) => {
+    return num * num;
+}
+
+//esta es una arrow function simplificada
+const square = num => num * num;
