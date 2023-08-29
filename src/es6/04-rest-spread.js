135209a2389ed12b72619863d1bcb9d0dc74becf
Randy Moya <stevenmoya02@gmail.com>
2023-08-28 20:42:24 -0600

    Se agrego contenido al archivo 04 sobre el spread operator

--- src/es6/04-rest-spread.js
+++ src/es6/04-rest-spread.js
@@ -3,7 +3,6 @@ let fruits = ['Apple', 'Banana'];
 let [a, b] = fruits;
 console.log(a, b);
 
-
 // Object destructuring
 let user = {
     username: 'Oscar',
@@ -11,3 +10,23 @@ let user = {
 }
 let {username, age} = user;
 console.log(username, user.age);
+
+// spread operator
+let person = {
+    name: 'Ana',
+    age: 65
+}
+let counrty = 'Estonia';
+
+let data = {...person, counrty};
+console.log(data);
+
+// rest
+function sum(num, ...values){
+    console.log(values);
+    console.log(num + values[0]);
+    return num + values[0];
+}
+
+sum(1, 1, 2, 3, 4);
+
