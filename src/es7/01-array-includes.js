e5540bdfd18364a2759a856a259f73a035091bf4
Randy Moya <stevenmoya02@gmail.com>
2023-08-31 12:14:48 -0600

    Se agrego el archivo 01 a es7 donde se vio el includes de javascript

---
+++ src/es7/01-array-includes.js
@@ -0,0 +1,9 @@
+let numbers = [1, 2, 3, 4, 7, 8, 9];
+
+console.log(numbers.includes(4)); // true
+console.log(numbers.includes(5)); // false
+
+const nameList = ['Pedro', 'Lucas', 'Amanda'];
+console.log(nameList.includes('pedro')); // true
+console.log(nameList.includes('Pedro')); // false
+
