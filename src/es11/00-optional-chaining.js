9b8d3ceeadae69c7423a921ab3748f0900826e85
Randy Moya <stevenmoya02@gmail.com>
2023-09-01 17:43:10 -0600

    Se agrego el archivo 00 a es11 donde se vio el uso del optional chaining

---
+++ src/es11/00-optional-chaining.js
@@ -0,0 +1,14 @@
+const users = {
+    gndx: {
+        country: 'Alemania'
+    },
+    ana: {
+        country: 'Colombia'
+    },
+    pedro: {
+        country: 'Mexico'
+    }
+}
+
+console.log(users.gndx.country);
+console.log(users?.bebeloper?.country);
